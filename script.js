8e20328dc5e5cf54641c63ee73c30a7751d77d77
AyeshaMalak <ayeshamalak34@gmail.com>
2024-10-20 12:04:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let paragraph =document.getElementById('para');
+paragraph.style.backgroundColor ="black";
+paragraph.style.color ="red";
+paragraph.style.height="200px";
+paragraph.style.width = "100px";
+paragraph.style.boder="1px solid black"
